205987f073963e6d43fcd5da9851ca47dd743b4f
jonaranjo <jonaranjo@HA-16.ec.tec.ac.cr>
2016-10-10 12:10:54 -0600

    Clase de entrada del proyecto

--- SoccerVideoProcessing/src/com/soccergame_main/Main.java
+++ SoccerVideoProcessing/src/com/soccergame_main/Main.java
@@ -1,10 +1,11 @@
-package com.videoprocessing;
+package com.soccergame_main;
 
 import org.opencv.core.Core;
 
 public class Main 
 {
-	public static void main (String args[]){
+	public static void main (String args[])
+	{
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 	}
 }
