cab05eb3f7fda1a1ee01704fe7d14733e22209f8
jonaranjo <jonaranjo@HA-16.ec.tec.ac.cr>
2016-10-10 12:05:26 -0600

    Librería OpenCV, versión 3.1, agregada al proyecto.

---
+++ SoccerVideoProcessing/src/com/videoprocessing/Main.java
@@ -0,0 +1,10 @@
+package com.videoprocessing;
+
+import org.opencv.core.Core;
+
+public class Main 
+{
+	public static void main (String args[]){
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+}
